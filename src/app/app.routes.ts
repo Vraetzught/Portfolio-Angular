021a537faaf0c87a496792ae687aa23bb0193611
Klaas B. <134652135+Vraetzught@users.noreply.github.com>
2025-05-22 22:11:17 +0200

    update routing configuration to include menu titles; enhance navbar links with active state styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import {
-  ContactPageComponent,
-  ExperiencePageComponent,
-  HobbiesPageComponent,
-  HomePageComponent,
-  ProjectsPageComponent,
+   ContactPageComponent,
+   ExperiencePageComponent,
+   HobbiesPageComponent,
+   HomePageComponent,
+   ProjectsPageComponent,
 } from './pages';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent, title: 'Home' },
-  { path: 'projects', component: ProjectsPageComponent, title: 'Projects' },
-  { path: 'ervaring', component: ExperiencePageComponent, title: 'Ervaring' },
-  { path: 'hobbies', component: HobbiesPageComponent, title: 'Hobbies' },
-  { path: 'contact', component: ContactPageComponent, title: 'Contact' },
+  { path: '', component: HomePageComponent, title: 'NB Portfolio - Home', data:{ menuTitle: 'Home' } },
+  { path: 'projects', component: ProjectsPageComponent, title: 'NB Portfolio - Projects', data:{ menuTitle: 'Projects' } },
+  { path: 'ervaring', component: ExperiencePageComponent, title: 'NB Portfolio - Ervaring', data:{ menuTitle: 'Ervaring' } },
+  { path: 'hobbies', component: HobbiesPageComponent, title: 'NB Portfolio - Hobbies', data:{ menuTitle: 'Hobbies' } },
+  { path: 'contact', component: ContactPageComponent, title: 'NB Portfolio - Contact', data:{ menuTitle: 'Contact' } },
 
 ];
--- src/app/ui-components/navbar/navbar.component.ts
+++ src/app/ui-components/navbar/navbar.component.ts
@@ -2,53 +2,54 @@ import { Component, ElementRef, EventEmitter, HostListener, inject, Output } fro
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { matDarkModeOutline, matLightModeOutline } from '@ng-icons/material-icons/outline';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { faSolidBars } from '@ng-icons/font-awesome/solid';
 
 @Component( {
-  selector: 'app-navbar',
-  imports: [
-    NgOptimizedImage,
-    CommonModule,
-    NgIcon,
-    RouterLink,
-  ],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
-  viewProviders: [ provideIcons( { matLightModeOutline, matDarkModeOutline, faSolidBars } ) ],
+   selector: 'app-navbar',
+   imports: [
+      NgOptimizedImage,
+      CommonModule,
+      NgIcon,
+      RouterLink,
+      RouterLinkActive,
+   ],
+   templateUrl: './navbar.component.html',
+   styleUrl: './navbar.component.css',
+   viewProviders: [ provideIcons( { matLightModeOutline, matDarkModeOutline, faSolidBars } ) ],
 } )
 export class NavbarComponent {
-  router = inject( Router );
-  elementRef = inject(ElementRef)
+   router = inject( Router );
+   elementRef = inject( ElementRef );
 
-  isDarkMode: boolean = false;
-  isMenuOpen: boolean = false;
+   isDarkMode: boolean = false;
+   isMenuOpen: boolean = false;
 
-  @Output() themeChangedEvent = new EventEmitter<boolean>;
+   @Output() themeChangedEvent = new EventEmitter<boolean>;
 
-  links = this.router.config
-    .filter( route => route.title && route.title !== undefined );
+   links = this.router.config
+      .filter( route => route.title );
 
 
-  toggleDarkMode () {
-    this.isDarkMode = !this.isDarkMode;
-    this.themeChangedEvent.emit( this.isDarkMode );
-  }
+   toggleDarkMode () {
+      this.isDarkMode = !this.isDarkMode;
+      this.themeChangedEvent.emit( this.isDarkMode );
+   }
 
-  toggleMenu () {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
+   toggleMenu () {
+      this.isMenuOpen = !this.isMenuOpen;
+   }
 
-  @HostListener('document:click', ['$event'])
-  clickOutside ( event: Event ) {
-    if (!this.isMenuOpen) return
+   @HostListener( 'document:click', [ '$event' ] )
+   clickOutside ( event: Event ) {
+      if ( !this.isMenuOpen ) return;
 
-    const target = event.target as HTMLElement;
-    const menuElement = this.elementRef.nativeElement.querySelector('.nav-menu');
-    const toggleButtonElement = this.elementRef.nativeElement.querySelector('[data-menu-toggle]');
+      const target = event.target as HTMLElement;
+      const menuElement = this.elementRef.nativeElement.querySelector( '.nav-menu' );
+      const toggleButtonElement = this.elementRef.nativeElement.querySelector( '[data-menu-toggle]' );
 
-    if (!menuElement?.contains(target) && !toggleButtonElement?.contains(target)) {
-      this.isMenuOpen = false;
-    }
-  }
+      if ( !menuElement?.contains( target ) && !toggleButtonElement?.contains( target ) ) {
+         this.isMenuOpen = false;
+      }
+   }
 }
