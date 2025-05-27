9829db7aa322e766f50d89b74454a4b4afaffe3c
Klaas B. <134652135+Vraetzught@users.noreply.github.com>
2025-05-27 20:02:06 +0200

    format hobbies page image arrays for improved readability; adjust indentation and structure

--- src/app/pages/hobbies-page/hobbies-page.component.ts
+++ src/app/pages/hobbies-page/hobbies-page.component.ts
@@ -12,25 +12,58 @@ import { ImageInfo } from '../../../interfaces';
 } )
 export class HobbiesPageComponent {
    motorcycleImages: ImageInfo[] = [
-      { path: 'assets/hobbies/motorcycles/yamaha_sr125.jpg', alt: 'Yamaha SR125 2001 - mijn eerste motor' },
-      { path: 'assets/hobbies/motorcycles/Honda_Pan.jpg', alt: 'Honda ST1100 Pan European 2001 - mijn eerste grote motor' },
-      { path: 'assets/hobbies/motorcycles/Kawasaki2.jpg', alt: 'Kawasaki Versys 1000 GT 2018 - mijn huidige motor' },
-      { path: 'assets/hobbies/motorcycles/me_on_bike.jpg', alt: 'Klaar voor een rit met de bezoekers van de Laverda Oldtimer Meeting in Hoogstraten' },
-      { path: 'assets/hobbies/motorcycles/Lier_ride_MotoBE.jpg', alt: 'Tussenstop tijdens de groepsrit rondom Lier, georganiseerd door leden van de MotoBE discord groep' },
+      {
+         path: 'assets/hobbies/motorcycles/yamaha_sr125.jpg',
+         alt: 'Yamaha SR125 2001 - mijn eerste motor',
+      },
+      {
+         path: 'assets/hobbies/motorcycles/Honda_Pan.jpg',
+         alt: 'Honda ST1100 Pan European 2001 - mijn eerste grote motor',
+      },
+      {
+         path: 'assets/hobbies/motorcycles/Kawasaki2.jpg',
+         alt: 'Kawasaki Versys 1000 GT 2018 - mijn huidige motor',
+      },
+      {
+         path: 'assets/hobbies/motorcycles/me_on_bike.jpg',
+         alt: 'Klaar voor een rit met de bezoekers van de Laverda Oldtimer Meeting in Hoogstraten',
+      },
+      {
+         path: 'assets/hobbies/motorcycles/Lier_ride_MotoBE.jpg',
+         alt: 'Tussenstop tijdens de groepsrit rondom Lier, georganiseerd door leden van de MotoBE discord groep',
+      },
    ];
 
    musicImages: ImageInfo[] = [
-      { path: 'assets/hobbies/music/clarinet.jpg', alt: 'Ik speelde 14 jaar klarinet in de Koninklijke Harmonie Sint Maurus in Holsbeek' },
-      { path: 'assets/hobbies/music/hsm.jpg', alt: 'Jaarlijks concert van de Koninklijke Harmonie Sint Maurus van Holsbeek' },
-      { path: 'assets/hobbies/music/equalizer.jpg', alt: 'Ik luister naar allerlei soorten muziek, van rustgevende atmosferische muziek, tot het zwaardere gitaarwerk.' },
-      { path: 'assets/hobbies/music/Na_Fir_Bolg.jpg', alt: 'Mijn vriendin en ik op het Folkfestival Na Fir Bolg in Vorselaar' },
-   ]
+      {
+         path: 'assets/hobbies/music/clarinet.jpg',
+         alt: 'Ik speelde 14 jaar klarinet in de Koninklijke Harmonie Sint Maurus in Holsbeek',
+      },
+      {
+         path: 'assets/hobbies/music/hsm.jpg',
+         alt: 'Jaarlijks concert van de Koninklijke Harmonie Sint Maurus van Holsbeek',
+      },
+      {
+         path: 'assets/hobbies/music/equalizer.jpg',
+         alt: 'Ik luister naar allerlei soorten muziek, van rustgevende atmosferische muziek, tot het zwaardere gitaarwerk.',
+      },
+      {
+         path: 'assets/hobbies/music/Na_Fir_Bolg.jpg',
+         alt: 'Mijn vriendin en ik op het Folkfestival Na Fir Bolg in Vorselaar',
+      },
+   ];
 
    muayThaiImages: ImageInfo[] = [
-      { path: 'assets/hobbies/muay-thai/dvoted-logo.jpg', alt: 'Januari 2025 ben ik terug beginnen trainen, dit keer bij DVoted in Heist-o/d-Berg' },
+      {
+         path: 'assets/hobbies/muay-thai/dvoted-logo.jpg',
+         alt: 'Januari 2025 ben ik terug beginnen trainen, dit keer bij DVoted in Heist-o/d-Berg',
+      },
       { path: 'assets/hobbies/muay-thai/dvoted-gym.jpg', alt: 'De nieuwe trainingsruimte van DVoted Heist-o/d-Berg' },
-      { path: 'assets/hobbies/muay-thai/training-session.jpg', alt: 'Een Muay Thai trainingssessie bij DVoted Heist-o/d-Berg' },
-   ]
+      {
+         path: 'assets/hobbies/muay-thai/training-session.jpg',
+         alt: 'Een Muay Thai trainingssessie bij DVoted Heist-o/d-Berg',
+      },
+   ];
 
 
 }
