8170f2208a1e549ecd3c561816d8e433a91c711e
Klaas B. <134652135+Vraetzught@users.noreply.github.com>
2025-05-29 10:45:06 +0200

    feat: add Not Found page component with redirect functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,34 @@ import {
    HomePageComponent,
    ProjectsPageComponent,
 } from './pages';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent, title: 'NB Portfolio - Home', data:{ menuTitle: 'Home' } },
-  { path: 'projects', component: ProjectsPageComponent, title: 'NB Portfolio - Projects', data:{ menuTitle: 'Projects' } },
-  { path: 'ervaring', component: ExperiencePageComponent, title: 'NB Portfolio - Ervaring', data:{ menuTitle: 'Ervaring' } },
-  { path: 'hobbies', component: HobbiesPageComponent, title: 'NB Portfolio - Hobbies', data:{ menuTitle: 'Hobbies' } },
-  { path: 'contact', component: ContactPageComponent, title: 'NB Portfolio - Contact', data:{ menuTitle: 'Contact' } },
+   { path: '', component: HomePageComponent, title: 'NB Portfolio - Home', data: { menuTitle: 'Home' } },
+   {
+      path: 'projects',
+      component: ProjectsPageComponent,
+      title: 'NB Portfolio - Projects',
+      data: { menuTitle: 'Projects' },
+   },
+   {
+      path: 'ervaring',
+      component: ExperiencePageComponent,
+      title: 'NB Portfolio - Ervaring',
+      data: { menuTitle: 'Ervaring' },
+   },
+   {
+      path: 'hobbies',
+      component: HobbiesPageComponent,
+      title: 'NB Portfolio - Hobbies',
+      data: { menuTitle: 'Hobbies' },
+   },
+   {
+      path: 'contact',
+      component: ContactPageComponent,
+      title: 'NB Portfolio - Contact',
+      data: { menuTitle: 'Contact' },
+   },
+   { path: `**`, component: NotFoundPageComponent, title: `404 - Page Not Found` },
 
 ];
